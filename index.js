0bc0a8055b9479dd871f5dde880a715a4d53afcd
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-04-30 16:43:31 +0300

    add

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const colors = [
   '#009688',
   '#795548',
 ];
+const DELAY = 1000;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -17,17 +18,30 @@ const refs = {
     bodyStyle: document.querySelector('body'),
 };
 
+refs.starBtn.addEventListener('click', (e) => {
+    e.target.disabled = true;
+    randomColor()
+});
+
+refs.stopBtn.addEventListener('click', (e) => {
+    refs.starBtn.disabled = false;
+    clearInterval(interval);
+
+} )
+
 function setBodyColor(colors) {
     refs.bodyStyle.style.backgroundColor = colors;
+    
 };
+
+const interval = setInterval(randomColor, DELAY, DELAY);
 
 function randomColor() {
     let randomNum = randomIntegerFromInterval(1, 6);
-    return colors.map((col, index) => {
+    let newColor = colors.map((col, index) => {
         if (randomNum === index) {
             setBodyColor(col);
         }
     })
-};
-
-randomColor();
+    return newColor;
+}
